269e29778dde640e3997108918b737c0f2da7571
praveen10100000 <praveenpandey.uttarakhand@gmail.com>
2022-05-11 13:43:59 +0530

    changes

--- src/main/java/com/codewithpp/JavaDriver/JavaDriverApplication.java
+++ src/main/java/com/codewithpp/JavaDriver/JavaDriverApplication.java
@@ -1,11 +1,15 @@
 package com.codewithpp.JavaDriver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavaDriverApplication {
 
+	@Value("${spring.datasource.url}")
+	static
+	String db;
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDriverApplication.class, args);
 	}
--- src/main/java/com/codewithpp/JavaDriver/entity/Ride.java
+++ src/main/java/com/codewithpp/JavaDriver/entity/Ride.java
@@ -1,5 +1,6 @@
 package com.codewithpp.JavaDriver.entity;
 
+import com.codewithpp.JavaDriver.otp.OtpDetails;
 import com.codewithpp.JavaDriver.util.RideStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,6 +25,7 @@ public class Ride {
     private int userId;
     private RideStatus status = RideStatus.Initiate;
     private double distance;
+//    private OtpDetails
 
     @ManyToOne
     @JoinColumn(name = "driver_id", referencedColumnName = "id")
